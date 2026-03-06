1e2b732d5bfbf0e9e161c6c2092de093770bdcb9
knaut <daniel@smallinvisiblemachines.com>
2026-03-05 20:27:56 -0800

    multiple switch blocks

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -11,9 +11,69 @@ import { css } from '@emotion/react'
 const ledW = 20;
 const ledH = 12;
 
+interface SwitchableState {
+	toggle: boolean
+	value: any
+}
+
+function collateSwitchableData(nodesData) {
+	const arr: SwitchableState[] = []
+
+	for (let i = 0; i < nodesData.length; i++ ) {
+		
+	}
+}
+
+function SwitchBlock({ data }) {
+	const [toggle, setToggle] = useState<SwitchableState>()
+
+	const handleToggle = useCallback(() => {
+		setToggle(!toggle)
+	})
+
+	console.log({data})
+
+	return (
+		<div css={css`
+			display: flex;
+			flex-direction: row;
+		`}>
+			<div css={css`
+				width: 50%;
+			`}>
+				<button onClick={handleToggle}>{toggle ? 'on' : 'off'}</button>
+			</div>
+			<div css={css`
+				width: 50%;
+				justify-content: center;
+				align-content: center;
+			`}>
+				<div css={css`
+					position: relative;
+					width: 100%;
+					&::before {
+						content: '';
+						display: block;
+						position: absolute;
+						top: calc(50% - ${ledH / 2}px);
+						left: calc(50% - ${ledW}px);
+						height: ${ledH}px;
+						width: ${ledW}px;
+						background: ${toggle ? "var(--cyan-bright)" : "var(--green-dark)"};
+						box-shadow: ${toggle ? "0 0 10px var(--cyan-bright)" : "0 0 0 #000"};
+						box-sizing: border-box;
+						border: 2px solid rgba(0,0,0,0.3);
+						border-radius: 30px;
+					}						}
+				`}/>
+			</div>
+		</div>
+	)
+}
+
 // a node that passes through values when a button is toggled
 export function Switch({id, data}) {
-	const [toggle, setToggle] = useState()
+	const [toggle, setToggle] = useState<SwitchableState[]>([])
 	const { updateNodeData } = useReactFlow()
 
 	const connections = useNodeConnections({
@@ -25,20 +85,27 @@ export function Switch({id, data}) {
 	console.log({connections, nodesData})
 
 
-	const handleToggle = useCallback(event => {
-		if (toggle) {
-			setToggle(false)
-			updateNodeData(id, { value: false })
-		} else {
-			setToggle(true)
-			nodesData.forEach(node => {
-				if (node?.id !== id) {
-					const value = node?.data?.value
-					updateNodeData(id, { toggle, value })
-				}
-			})
-		}
-	}, [id, toggle, setToggle])
+	// const handleToggle = useCallback(event => {
+	// 	if (toggle) {
+	// 		setToggle(false)
+	// 		// updateNodeData(id, { value: false })
+	// 	} else {
+	// 		setToggle(true)
+	// 		// nodesData.forEach(node => {
+	// 		// 	if (node?.id !== id) {
+	// 		// 		const value = node?.data?.value
+	// 		// 		updateNodeData(id, { toggle, value })
+	// 		// 	}
+	// 		// })
+	// 	}
+	// }, [id, toggle, setToggle])
+
+	// useEffect(() => {
+	// 	console.log('useEffect toggle changed')
+	// 	if (toggle) {
+			
+	// 	}
+	// }, [toggle])
 
 	// useEffect(() => {
 	// 	console.log('useEffect', sources)
@@ -53,37 +120,11 @@ export function Switch({id, data}) {
 			<label>Switch</label>
 			<Handle type="target" position={Position.Left} />
 			<div css={css`
-				display: flex
+				display: flex;
+				flex-direction: column;
+				gap: 8px;
 			`}>
-				<div css={css`
-					width: 50%;
-				`}>
-					<button onClick={handleToggle}>{toggle ? 'on' : 'off'}</button>
-				</div>
-				<div css={css`
-					width: 50%;
-					justify-content: center;
-					align-content: center;
-				`}>
-					<div css={css`
-						position: relative;
-						width: 100%;
-						&::before {
-							content: '';
-							display: block;
-							position: absolute;
-							top: calc(50% - ${ledH / 2}px);
-							left: calc(50% - ${ledW}px);
-							height: ${ledH}px;
-							width: ${ledW}px;
-							background: ${toggle ? "var(--cyan-bright)" : "var(--green-dark)"};
-							box-shadow: ${toggle ? "0 0 10px var(--cyan-bright)" : "0 0 0 #000"};
-							box-sizing: border-box;
-							border: 2px solid rgba(0,0,0,0.3);
-							border-radius: 30px;
-						}						}
-					`}/>
-				</div>
+				{nodesData.map((n, i) => <SwitchBlock id={n.id} data={n.data} key={i}/>)}
 			</div>
 			<Handle type="source" position={Position.Right} />
 		</div>
