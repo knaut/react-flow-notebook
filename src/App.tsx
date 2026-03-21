f56eda12f936c2dbbbcbe16e6c50481faebda320
knaut <daniel@smallinvisiblemachines.com>
2026-03-20 18:16:18 -0700

    working color picker node w/ radix dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import {
 } from '@xyflow/react'
 import '@xyflow/react/dist/style.css'
 import './additional.css'
+import '@radix-ui/themes/styles.css'
 
 import { NumberInput } from './components/NumberInput'
 import { RandomNumber } from './components/RandomNumber'
@@ -24,7 +25,7 @@ import { ColorPicker } from './components/ColorPicker'
 import { getInitialNodes, getInitialEdges } from './utils'
 import { NODE_TYPES, EDGE_TYPES } from './constants'
 
-
+import { Theme as RadixTheme } from '@radix-ui/themes'
 
 export default function App() {
   const [nodes, setNodes] = useState(getInitialNodes())
@@ -52,34 +53,36 @@ export default function App() {
   )
 
   return (
-    <div data-color-mode='dark' style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow
-        colorMode="dark"
-        connectionLineType="smoothstep"
-        nodeTypes={{
-          [NODE_TYPES.TEXT_UPDATER]: TextUpdaterNode,
-          [NODE_TYPES.NUMBER_INPUT]: NumberInput,
-          [NODE_TYPES.RANDOM_NUMBER]: RandomNumber,
-          [NODE_TYPES.COMBINE]: CombineNode,
-          [NODE_TYPES.SWITCH]: Switch,
-          [NODE_TYPES.THEME]: Theme,
-          [NODE_TYPES.COLOR_PICKER]: ColorPicker,
-        }}
-        edgeTypes={{
-          [EDGE_TYPES.EXAMPLE_CUSTOM_EDGE]: ExampleCustomEdge
-        }}
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        fitView
-      >
-        <Background/>
-        <Controls/>
-        <MiniMap nodeColor="#50F2E840"/>
-      </ReactFlow>
-    </div>
+    <RadixTheme>
+      <div data-color-mode='dark' style={{ width: '100vw', height: '100vh' }}>
+        <ReactFlow
+          colorMode="dark"
+          connectionLineType="smoothstep"
+          nodeTypes={{
+            [NODE_TYPES.TEXT_UPDATER]: TextUpdaterNode,
+            [NODE_TYPES.NUMBER_INPUT]: NumberInput,
+            [NODE_TYPES.RANDOM_NUMBER]: RandomNumber,
+            [NODE_TYPES.COMBINE]: CombineNode,
+            [NODE_TYPES.SWITCH]: Switch,
+            [NODE_TYPES.THEME]: Theme,
+            [NODE_TYPES.COLOR_PICKER]: ColorPicker,
+          }}
+          edgeTypes={{
+            [EDGE_TYPES.EXAMPLE_CUSTOM_EDGE]: ExampleCustomEdge
+          }}
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          fitView
+        >
+          <Background/>
+          <Controls/>
+          <MiniMap nodeColor="#50F2E840"/>
+        </ReactFlow>
+      </div>
+    </RadixTheme>
   )
 
 }
--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useState, useEffect } from 'react'
 import { css } from '@emotion/react'
 import { Handle, Position, useReactFlow } from '@xyflow/react'
 import { randomInteger } from '../utils'
@@ -8,13 +8,19 @@ import { Slider, Sketch, Material, Colorful, Compact, Circle, Swatch, Wheel, Blo
 import { Alpha, Hue, ShadeSlider, Saturation, hsvaToHslaString } from '@uiw/react-color';
 import { EditableInput, EditableInputRGBA, EditableInputHSLA } from '@uiw/react-color';
 
+import { DropdownMenu } from 'radix-ui'
+
 export function ColorPicker({ id, value }) {
 	const { updateNodeData } = useReactFlow()
-	const [hex, setHex] = useState("#fff");
+	const [hex, setHex] = useState("#fff" || value);
 
 	const onColorChange = useCallback(color => {
 		setHex(color.hex)
 		updateNodeData(id, { value: color.hex })
+	}, [])
+
+	useEffect(() => {
+		updateNodeData(id, { value: hex})
 	}, [])
 
 	return (
@@ -23,10 +29,31 @@ export function ColorPicker({ id, value }) {
 
 			<Handle type="source" position={Position.Right} />
 			<div className="nodrag">
-				<Chrome
-		      color={hex}
-		      onChange={onColorChange}
-		    />
+				<DropdownMenu.Root>
+					<DropdownMenu.Trigger asChild>
+						<button css={css`
+								padding: 4px !important;
+							`}>
+							<div css={css`
+								background: ${hex};
+								height: 30px;
+								width: 100%;
+								border-radius: 3px;
+							`}>
+								<span css={css`display:none`}>Color</span>
+							</div>
+						</button>
+				  </DropdownMenu.Trigger>
+
+				  <DropdownMenu.Portal>
+				  	<DropdownMenu.Content>
+				  		<Chrome
+					      color={hex}
+					      onChange={onColorChange}
+					    />
+				  	</DropdownMenu.Content>
+				  </DropdownMenu.Portal>
+			  </DropdownMenu.Root>
 		  </div>
 			<div>{value}</div>
 		</div>
